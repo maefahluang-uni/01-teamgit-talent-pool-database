ff7ba3418baaeca4424a52d322ecfb06b787e30b
PHYO HAN TUN - <6431503128@lamduan.mfu.ac.th>
2024-01-19 19:23:59 +0700

    Task 1 - done

--- src/main/java/util/Counter.java
+++ src/main/java/util/Counter.java
@@ -2,33 +2,32 @@ package util;
 
 import java.util.StringTokenizer;
 
-//This is an example (faulty) class to be used for A4 Task1 and Task2
 public class Counter {
-	public int _ctr;
-	public int _multiplier;
+    public int _ctr;
+    public int _multiplier;
 
-	// method increments _ctr
-	public void increment() {
-		_ctr--; // faulty increment
-	}
+    // method increments _ctr
+    public void increment() {
+        _ctr++; // corrected increment
+    }
 
-	// method decrements _ctr
-	public void decrement() {
-		_ctr++; // faulty decrement
-	}
+    // method decrements _ctr
+    public void decrement() {
+        _ctr--; // corrected decrement
+    }
 
-	// method resets _ctr
-	public void reset() {
-		_ctr = 1; // faulty reset, _ctr should rather be 0
-	}
+    // method resets _ctr
+    public void reset() {
+        _ctr = 0; // corrected reset, _ctr should be 0
+    }
 
-	// method multiplies _ctr by n
-	public void multiplyBy(int multiplier) {
-		_multiplier = multiplier; // no fault here
-		_ctr = _ctr * _multiplier; // no fault here
-	}
+    // method multiplies _ctr by n
+    public void multiplyBy(int multiplier) {
+        _multiplier = multiplier; // no fault here
+        _ctr = _ctr * _multiplier; // no fault here
+    }
 
-	// TODO: dev1- method for increment to closest even number
+    // TODO: dev1- method for increment to closest even number
 	public void incrementToEven() {
 		_ctr = -99;
 	}
